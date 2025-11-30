08d62453606b2c8c81d6bebdf8cf6558c11ff64f
ganea-andrei-dragos-30321 <Ganea.Ma.Andrei@student.utcluj.ro>
2025-11-30 14:31:55 +0200

    Slip Control code generated

---
+++ work/Slip_Control_CodeGen_ert_rtw/Slip_Control_CodeGen_data.cpp
@@ -0,0 +1,59 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: Slip_Control_CodeGen_data.cpp
+//
+// Code generated for Simulink model 'Slip_Control_CodeGen'.
+//
+// Model version                  : 15.43
+// Simulink Coder version         : 25.2 (R2025b) 28-Jul-2025
+// C/C++ source code generated on : Sun Nov 30 14:28:26 2025
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Atmel->AVR
+// Code generation objectives:
+//    1. Execution efficiency
+//    2. RAM efficiency
+// Validation result: Not run
+//
+#include "Slip_Control_CodeGen.h"
+
+// Constant parameters (default storage)
+const Slip_Control_CodeGen::ConstP rtConstP{
+  // Pooled Parameter (Expression: MotorT)
+  //  Referenced by:
+  //    '<S2>/1-D Lookup Table'
+  //    '<S2>/1-D Lookup Table1'
+
+  { 51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 51.0306904761905, 51.0306904761905, 51.0306904761905,
+    51.0306904761905, 44.843689833269, 36.8215688218961, 30.062728824122,
+    25.3370743140982, 21.0374123095361, 17.0840324267782, 13.4184887499186,
+    9.99592684833052, 6.77696534591492, 3.73957484604455 },
+
+  // Pooled Parameter (Expression: MotorW)
+  //  Referenced by:
+  //    '<S2>/1-D Lookup Table'
+  //    '<S2>/1-D Lookup Table1'
+
+  { 0.0, 100.0, 200.0, 300.0, 400.0, 500.0, 600.0, 700.0, 800.0, 900.0, 1000.0,
+    1100.0, 1200.0, 1300.0, 1400.0, 1500.0, 1600.0, 1700.0, 1800.0, 1900.0,
+    2000.0, 2100.0, 2200.0, 2300.0, 2400.0, 2500.0, 2600.0, 2700.0, 2800.0,
+    2900.0, 3000.0, 3100.0, 3200.0, 3300.0, 3400.0, 3500.0, 3600.0, 3700.0,
+    3800.0, 3900.0, 4000.0, 4100.0, 4200.0, 4300.0, 4400.0, 4500.0, 4600.0 }
+};
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
